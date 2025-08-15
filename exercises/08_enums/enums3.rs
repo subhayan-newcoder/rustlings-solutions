56e11933bbb70e8d3d45b05031ec7ac5a3eb8c00
subhayan-newcoder <mail4subhayan@gmail.com>
2025-08-15 14:00:08 +0530

    strings and enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(p) => self.move_position(p),
+            Message::ChangeColor(a,b,c ) => self.change_color(a, b, c),
+            Message::Echo(s) => self.echo(s),
+            Message::Quit => self.quit()
+        }
     }
 }
 
