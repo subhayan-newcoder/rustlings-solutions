0521fa3ffc98cd50f91ccfd44914b774aaa21d31
subhayan-newcoder <mail4subhayan@gmail.com>
2025-08-16 09:04:21 +0530

    upto hashamap

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,19 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+
+        let default_team_score = TeamScores {goals_scored : 0, goals_conceded: 0};
+        
+        let team_1_old_score = scores.get(team_1_name).unwrap_or(&default_team_score);
+        let team_2_old_score = scores.get(team_2_name).unwrap_or(&default_team_score);
+
+
+        let team_1_new_score = TeamScores{ goals_scored : (team_1_old_score.goals_scored + team_1_score), goals_conceded: (team_1_old_score.goals_conceded + team_2_score) };
+        let team_2_new_score = TeamScores{ goals_scored : (team_2_old_score.goals_scored + team_2_score), goals_conceded: (team_2_old_score.goals_conceded + team_1_score) };
+
+        scores.insert(team_1_name, team_1_new_score);
+        scores.insert(team_2_name, team_2_new_score);
+
     }
 
     scores
